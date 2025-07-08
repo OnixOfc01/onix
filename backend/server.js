2a67523f886fb65e72364ad5040dd0eed871cd8a
OnixOfc01 <ofconixcenter@gmail.com>
2025-07-08 18:32:31 -0500

    Eliminar URI duplicada y corregir conexión MongoDB

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// Configuración de la conexión a MongoDB Atlas
+
 const uri = "mongodb+srv://onix1751:onix1751@onix-proyect.5fgmndn.mongodb.net/onixdb?retryWrites=true&w=majority&appName=onix-proyect";
+
 
 const client = new MongoClient(uri, {
   serverApi: {
